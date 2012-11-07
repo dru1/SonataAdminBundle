5de88ee7f2438964d90894ed05eee9c04b6978c2
dru1 <d.rudigier@interwall.at>
2012-11-07 10:47:30 +0100

    expanded fix #2

--- Form/Type/TranslatableChoiceType.php
+++ Form/Type/TranslatableChoiceType.php
@@ -14,11 +14,19 @@ namespace Sonata\AdminBundle\Form\Type;
 
 use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceListInterface;
+use Symfony\Component\Form\Extension\Core\ChoiceList\ArrayChoiceList;
+use Symfony\Component\Form\Extension\Core\DataTransformer\ArrayToBooleanChoicesTransformer;
+use Symfony\Component\Form\Extension\Core\DataTransformer\ScalarToBooleanChoicesTransformer;
+use Symfony\Component\Form\Extension\Core\EventListener\FixRadioInputListener;
+use Symfony\Component\Form\Extension\Core\DataTransformer\ArrayToChoicesTransformer;
+use Symfony\Component\Form\Extension\Core\DataTransformer\ScalarToChoiceTransformer;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\AbstractType;
 
-class TranslatableChoiceType extends ChoiceType
+class TranslatableChoiceType extends AbstractType
 {
     protected $translator;
 
@@ -68,7 +76,61 @@ class TranslatableChoiceType extends ChoiceType
             $options['empty_value'] = $this->translator->trans($options['empty_value'], array(), $options['catalogue']);
         }
         
-        parent::buildForm($builder, $options);
+        // from the choice list itself
+        
+        if ($options['choice_list'] && !$options['choice_list'] instanceof ChoiceListInterface) {
+            throw new FormException('The "choice_list" must be an instance of "Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceListInterface".');
+        }
+
+        if (!$options['choice_list']) {
+            $options['choice_list'] = new ArrayChoiceList($options['choices']);
+        }
+
+        if ($options['expanded']) {
+            // Load choices already if expanded
+            $options['choices'] = $options['choice_list']->getChoices();
+
+            foreach ($options['choices'] as $choice => $value) {
+                if ($options['multiple']) {
+                    $builder->add((string) $choice, 'checkbox', array(
+                        'value'     => $choice,
+                        'label'     => $value,
+                        // The user can check 0 or more checkboxes. If required
+                        // is true, he is required to check all of them.
+                        'required'  => false,
+                    ));
+                } else {
+                    $builder->add((string) $choice, 'radio', array(
+                        'value' => $choice,
+                        'label' => $value,
+                    ));
+                }
+            }
+        }
+
+        // empty value
+        if ($options['multiple'] || $options['expanded']) {
+            // never use and empty value for these cases
+            $emptyValue = null;
+        } elseif (false === $options['empty_value']) {
+            // an empty value should be added but the user decided otherwise
+            $emptyValue = null;
+        } elseif (null === $options['empty_value']) {
+            // user did not made a decision, so we put a blank empty value
+            $emptyValue = $options['required'] ? null : '';
+        } else {
+            // empty value has been set explicitly
+            $emptyValue = $options['empty_value'];
+        }
+
+        $builder
+            ->setAttribute('choice_list', $options['choice_list'])
+            ->setAttribute('preferred_choices', $options['preferred_choices'])
+            ->setAttribute('multiple', $options['multiple'])
+            ->setAttribute('expanded', $options['expanded'])
+            ->setAttribute('required', $options['required'])
+            ->setAttribute('empty_value', $emptyValue)
+        ;
     }
 
     /**
@@ -76,7 +138,7 @@ class TranslatableChoiceType extends ChoiceType
      */
     public function getParent(array $options)
     {
-        return $options['expanded'] ? 'form' : 'field';
+        return 'choice';
     }
 
     /**
